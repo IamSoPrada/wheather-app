10eb08d8ed28c565c31d801436f0620af0a4ebb4
Rustem Tarasevich <tarasevicrustem@gmail.com>
2022-05-24 14:01:46 +0300

    Added types for node

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import * as path from "path";
 
-// https://vitejs.dev/config/
+import tsconfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
   plugins: [tsconfigPaths(), react()],
+  server: {
+    host: true,
+  },
 });
